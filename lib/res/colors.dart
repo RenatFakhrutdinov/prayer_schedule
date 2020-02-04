c0df78053511499e3e30b12b495a18f98b99c809
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-02-04 18:34:13 +0400

    prepare schedule screen

---
+++ lib/res/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color qiblaBlue = Color.fromRGBO(34, 141, 247, 1);
+
+  static const Color neutral = Color.fromRGBO(236, 234, 236, 1);
+
+  static const Color qiblaBlueTransparent = Color.fromRGBO(34, 141, 247, 0.5);
+}
---
+++ lib/ui/schedule_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatefulWidget {
+  @override
+  _ScheduleScreenState createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Prayer times'),
+      ),
+    );
+  }
+}
