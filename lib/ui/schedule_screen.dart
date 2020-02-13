aba588f770678a28e0d1cf297de43d5650f518e1
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-02-13 20:36:48 +0400

    ui of schedule

--- lib/ui/schedule_screen.dart
+++ lib/ui/schedule_screen.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prayer_schedule/bloc/location_bloc/location_bloc.dart';
 import 'package:prayer_schedule/bloc/location_bloc/location_bloc_state.dart';
 import 'package:prayer_schedule/bloc/prayer_time_bloc/prayer_time_export.dart';
+import 'package:prayer_schedule/res/colors.dart';
 
 class ScheduleScreen extends StatefulWidget {
   @override
@@ -66,7 +69,150 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
                   bloc: _prayerTimeBloc,
                   builder: (context, prayerState) {
                     if (prayerState is PrayerTimeLoaded) {
-                      return Text(prayerState.model.asr);
+                      return Column(
+                        children: <Widget>[
+                          Card(
+                            child: InkWell(
+                              onTap: () => print('tapped'),
+                              child: Padding(
+                                padding: EdgeInsets.all(8),
+                                child: Column(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.event,
+                                      color: AppColors.qiblaBlue,
+                                      size: ScreenUtil().setHeight(72),
+                                    ),
+                                    Text(
+                                      DateFormat.yMMMMd()
+                                          .format(DateTime.now()),
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Card(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          children: <Widget>[
+                                            Text('Sunrise'),
+                                            Text(
+                                              prayerState.model.sunrise,
+                                              style: TextStyle(
+                                                  color: Colors.redAccent),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Card(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          children: <Widget>[
+                                            Text('Sunset'),
+                                            Text(
+                                              prayerState.model.sunset,
+                                              style: TextStyle(
+                                                  color: Colors.redAccent),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Text("Fajr"),
+                                          Text(
+                                            prayerState.model.fajr,
+                                            style: TextStyle(
+                                                color: Colors.redAccent),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Text("Dhuhr"),
+                                          Text(
+                                            prayerState.model.dhuhr,
+                                            style: TextStyle(
+                                                color: Colors.redAccent),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Text("Asr"),
+                                          Text(
+                                            prayerState.model.asr,
+                                            style: TextStyle(
+                                                color: Colors.redAccent),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Text("Maghrib"),
+                                          Text(
+                                            prayerState.model.maghrib,
+                                            style: TextStyle(
+                                                color: Colors.redAccent),
+                                          )
+                                        ],
+                                      ),
+                                      Divider(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          Text("Isha"),
+                                          Text(
+                                            prayerState.model.isha,
+                                            style: TextStyle(
+                                                color: Colors.redAccent),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      );
                     } else if (prayerState is PrayerTimeError) {
                       return Text('something went wrong');
                     } else
